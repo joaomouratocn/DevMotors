3e35953a6dec1ac86a86b5ede00b4be57ed69d33
joao-mourato <joaomouratocn@gmail.com>
2025-07-29 16:55:36 -0300

    correção de import styles.scss

--- src/components/home/submenu/index.tsx
+++ src/components/home/submenu/index.tsx
@@ -1,14 +1,15 @@
 import Link from "next/link";
-import Styles from "./styles.modules.scss";
+import styles from "./styles.module.scss";
+import { X, Menu } from "lucide-react";
 
 export function Submenu() {
   return (
-    <section className={Styles.submenu}>
-      <ul>
+    <section className={styles.submenu}>
+      <ul className={styles.ul}>
         <li>
-          <Link href="/post/pg1"></Link>
-          <Link href="/post/pg2"></Link>
-          <Link href="/post/pg3"></Link>
+          <Link href="/post/pg1">Página 1</Link>
+          <Link href="/post/pg2">Página 2</Link>
+          <Link href="/post/pg3">Página 3</Link>
         </li>
       </ul>
     </section>
